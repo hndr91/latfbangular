9465c8b5933dea9764d77415cc3c8e9631da7a4b
Afif Hendrawan <afif.hendrawan@gmail.com>
2016-06-30 13:11:50 +0700

    home

--- app/app.js
+++ app/app.js
@@ -3,12 +3,11 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+  'myApp.home'
+])
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+.config(['$routeProvider', function($routeProvider){
+  $routeProvider.otherwise({
+        redirectTo: '/home'
+    });
 }]);
